cf364444b785771440844ba5cedf688876596741
Oscar Lee <N00230382@iadt.ie>
2024-12-13 20:13:56 +0000

    Added the many to many models but dont know how to impliment it as checkboxes instead of text

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\PhoneSeeder;
 use Carbon\Carbon;
@@ -18,9 +17,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(FeatureSeeder::class);
         $this->call(PhoneSeeder::class);
-
-       
-        //$this->call(ReviewSeeder::class);
+        $this->call(Feature_phone::class);
 
     
         User::factory()->create([
